569b0b8b9aec9ac5fd69d4a1985f489ec9447d32
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-02-20 12:58:47 +0100

    remove comments

--- multibranch/src/api.ts
+++ multibranch/src/api.ts
@@ -6,10 +6,10 @@ export const api = Router();
 api.get('/sum', async (req, res) => {
   try {
     const params = req.query;
-    console.log(params);
+    // console.log(params);
     const a: number = Number(params.a);
     const b: number = Number(params.b);
-    console.log(a, b);
+    // console.log(a, b);
     const result = `The result is ${sum(a, b)}`;
     res.send(result);
   } catch (error) {
